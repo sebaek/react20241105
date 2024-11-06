952c5e884bc73d55c17acb5a883fc08cfe130b15
sebaek <sebaek@gmail.com>
2024-11-06 14:09:22 +0900

    props

--- src/practice/App16.jsx
+++ src/practice/App16.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function App16(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyCom address={"ny"} city={"seoul"} score={9.88} />
+      <MyCom address={"ca"} city={"busan"} score={8.99} />
+    </div>
+  );
 }
+// MyCom 컴포넌트 작성
 
 export default App16;
