332edce56b4002648a6e870118f9a2956ce9e774
sebaek <sebaek@gmail.com>
2024-11-06 09:39:24 +0900

    jsx 작성법

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import MyApp4 from "./apps/MyApp4.jsx";
+import App4 from "./practice/App4.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <MyApp4 />
+    <App4 />
   </StrictMode>,
 );
