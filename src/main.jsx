a4aa7f2ff8de3a3b18fd381ad4de183782761568
sebaek <sebaek@gmail.com>
2024-11-06 11:43:10 +0900

    array filter map

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import MyApp10 from "./apps/MyApp10.jsx";
+import App10 from "./practice/App10.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <MyApp10 />
+    <App10 />
   </StrictMode>,
 );
---
+++ src/practice/App10.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function App10(props) {
+  const a = [
+    { name: "son", age: 30 },
+    { name: "rose", age: 10 },
+    { name: "faker", age: 40 },
+    { name: "hankang", age: 50 },
+    { name: "lee", age: 20 },
+    { name: "kim", age: 70 },
+  ];
+
+  // 30세 이상인 사람의 이름만 출력
+  /*
+  <h1>son</h1>
+  <h1>faker</h1>
+  <h1>hankang</h1>
+  <h1>kim</h1>
+   */
+
+  return <div></div>;
+}
+
+export default App10;
