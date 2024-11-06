f9b12cdda4cf39e707232d8217e3e3f17e49b81f
sebaek <sebaek@gmail.com>
2024-11-06 14:39:06 +0900

    props, destructuring

---
+++ src/practice/App18.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App18(props) {
+  return (
+    <div>
+      <Comp
+        product={{ name: "pizza", price: 3000 }}
+        title="소년이온다"
+        content="518"
+        author="한강"
+      />
+    </div>
+  );
+}
+
+export default App18;
