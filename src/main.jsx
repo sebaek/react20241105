921a938920e822c736542626515501ed4037e524
sebaek <sebaek@gmail.com>
2024-11-06 15:43:07 +0900

    children prop

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import MyApp22 from "./apps/MyApp22.jsx";
+import App22 from "./practice/App22.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <MyApp22 />
+    <App22 />
   </StrictMode>,
 );
