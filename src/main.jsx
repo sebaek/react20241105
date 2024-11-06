32d1c6e1820d568f83f4764d9320721db01e9b20
sebaek <sebaek@gmail.com>
2024-11-06 11:28:47 +0900

    list map

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import MyApp8 from "./apps/MyApp8.jsx";
+import MyApp9 from "./apps/MyApp9.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <MyApp8 />
+    <MyApp9 />
   </StrictMode>,
 );
