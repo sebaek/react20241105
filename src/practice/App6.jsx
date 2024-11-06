58a8b577c76a9b33becff783f339668c82dbb46c
sebaek <sebaek@gmail.com>
2024-11-06 10:32:42 +0900

    {} : jsx 내의 js코드

---
+++ src/practice/App6.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function App6(props) {
+  const a = "note";
+  const b = 123.456;
+  const c = {
+    name: "rose",
+    city: "seoul",
+  };
+
+  /*
+  <h1 class="note"> hello </h1>
+  <h3> note </h3>
+  <input type="number" step="0.001" value="123.456" />
+  <br />
+  <h3 class="seoul"> rose </h3>
+
+
+   */
+
+  return <div></div>;
+}
+
+export default App6;
