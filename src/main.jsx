66c52164ea75815ee6c38cede8535a00ff10b309
sebaek <sebaek@gmail.com>
2024-11-06 09:32:33 +0900

    jsx 작성법

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import MyApp3 from "./apps/MyApp3.jsx";
+import MyApp4 from "./apps/MyApp4.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <MyApp3 />
+    <MyApp4 />
   </StrictMode>,
 );
---
+++ src/practice/App3.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App3(props) {
+  return <div>세번째 화면</div>;
+}
+
+export default App3;
