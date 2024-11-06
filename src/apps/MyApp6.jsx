2b3fd6d865ca59076782cde610d17d201cf1fc19
sebaek <sebaek@gmail.com>
2024-11-06 10:21:17 +0900

    {} : jsx 내의 js코드

---
+++ src/apps/MyApp6.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+function MyApp6(props) {
+  return (
+    <div>
+      <Comp1 />
+    </div>
+  );
+}
+
+function Comp1() {
+  console.log("자바스크립트 코드");
+
+  // jsx 내에서 자바스크립트 사용 가능
+  // {}를 사용
+  // 출력용
+
+  const a = "hello";
+  const b = 3.14;
+  const c = ["son", "rose"];
+  const d = { name: "son", age: 30 };
+  return (
+    <div>
+      comp1
+      {console.log("jsx내의 js 코드")}
+      <h1>{a}</h1>
+      <h1>{b}</h1>
+      <h1>{c}</h1>
+      <h1>{d.name}</h1>
+      <h1>{d.age}</h1>
+      <h1>d.name</h1>
+    </div>
+  );
+}
+
+function Comp2() {
+  const a = "donald";
+  const b = 123.456;
+  const c = ["apt", "black", "pink"];
+  const d = {
+    name: "rose",
+    city: "seoul",
+  };
+
+  return (
+    <div>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+}
+
+export default MyApp6;
