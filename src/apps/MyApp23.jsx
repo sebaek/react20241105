f2d61ae28db793a2f5f9ff8a9055132f0b646410
sebaek <sebaek@gmail.com>
2024-11-06 16:21:49 +0900

    style prop

---
+++ src/apps/MyApp23.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+function MyApp23(props) {
+  return (
+    <div>
+      <h1
+        style={{
+          color: "yellow",
+          backgroundColor: "black",
+        }}
+      >
+        lorem3
+      </h1>
+      <p
+        style={{
+          paddingLeft: "50px",
+          border: "2px solid green",
+        }}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
+        architecto atque deserunt ea est facere laboriosam magni maiores minima
+        nam nihil porro possimus quae quam quibusdam recusandae, sequi suscipit
+        tenetur.
+      </p>
+      <ul
+        style={{
+          listStyleType: "cjk-decimal",
+          color: "blue",
+        }}
+      >
+        <li>Lorem.</li>
+        <li>Saepe!</li>
+        <li>Magni.</li>
+      </ul>
+    </div>
+  );
+}
+
+export default MyApp23;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App22 from "./practice/App22.jsx";
+import MyApp24 from "./apps/MyApp24.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App22 />
+    <MyApp24 />
   </StrictMode>,
 );
