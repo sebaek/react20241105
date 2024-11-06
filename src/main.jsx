b3cd2b9eaefe6a0baa1687fe5a9cb3ac5b6e0eb5
sebaek <sebaek@gmail.com>
2024-11-06 10:44:54 +0900

    {} : jsx 내의 js코드
    props 의 값으로 사용가능

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App6 from "./practice/App6.jsx";
+import MyApp7 from "./apps/MyApp7.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App6 />
+    <MyApp7 />
   </StrictMode>,
 );
