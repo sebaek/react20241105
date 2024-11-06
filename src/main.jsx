1749c5e4951817df3335b5f938a9745afb740c92
sebaek <sebaek@gmail.com>
2024-11-06 16:41:21 +0900

    chakra ui 맛보기

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App24 from "./practice/App24.jsx";
 import { Provider } from "./components/ui/provider.jsx";
+import MyApp25 from "./apps/MyApp25.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <App24 />
+      <MyApp25 />
     </Provider>
   </StrictMode>,
 );
