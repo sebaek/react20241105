4e1ac333f5251a6102e4cf62b1881a7fe83f8f54
sebaek <sebaek@gmail.com>
2024-11-07 14:40:50 +0900

    event 객체

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-import MyApp35 from "./apps/MyApp35.jsx";
+import App34 from "./practice/App34.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <MyApp35 />
+      <App34 />
       <Toaster />
     </Provider>
   </StrictMode>,
---
+++ src/practice/App34.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Box, HStack } from "@chakra-ui/react";
+
+function App34(props) {
+  return (
+    <div>
+      <HStack h={20}>
+        <Box h={20} bg={"orange"}>
+          Lorem ipsum dolor.
+        </Box>
+        <Box h={20} bg={"blue"}>
+          adipisicing elit. Ab?
+        </Box>
+        <Box h={20} bg={"teal"}>
+          adipisicing elit. Aliquam.
+        </Box>
+      </HStack>
+    </div>
+  );
+}
+
+export default App34;
