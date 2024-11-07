736d2d7d97558d5e67f5dd34e956e0bdaf5a1b48
sebaek <sebaek@gmail.com>
2024-11-07 14:30:38 +0900

    toaster

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
-import App33 from "./practice/App33.jsx";
+import MyApp34 from "./apps/MyApp34.jsx";
+import { Toaster } from "./components/ui/toaster.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <App33 />
+      <MyApp34 />
+      <Toaster />
     </Provider>
   </StrictMode>,
 );
