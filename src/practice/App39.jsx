3b75346835f128aa6fa742cf4c11d2fd4f9235aa
sebaek <sebaek@gmail.com>
2024-11-07 16:28:22 +0900

    state

---
+++ src/practice/App39.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Button } from "../components/ui/button.jsx";
+import { Box } from "@chakra-ui/react";
+
+function App39(props) {
+  return (
+    <div>
+      <Button>1</Button>
+      <Box h={20} bg={"blue.300"}>
+        1
+      </Box>
+      <Box h={20} bg={"red.300"}>
+        2
+      </Box>
+      <Box h={20} bg={"yellow.300"}>
+        3
+      </Box>
+    </div>
+  );
+}
+
+export default App39;
