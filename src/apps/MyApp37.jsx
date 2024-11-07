f244f7cd4ffcf94f83e3e28798fafb0cef2fa2da
sebaek <sebaek@gmail.com>
2024-11-07 15:33:18 +0900

    state

---
+++ src/apps/MyApp37.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { Button } from "../components/ui/button.jsx";
+import { Box } from "@chakra-ui/react";
+
+function MyApp37(props) {
+  const [number, setNumber] = useState(0);
+
+  function fun1() {
+    setNumber(number + 1);
+  }
+
+  return (
+    <div>
+      <Button onClick={fun1}>값 증가</Button>
+      <Button>값 감소</Button>
+      <Box>number : {number}</Box>
+    </div>
+  );
+}
+
+export default MyApp37;
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-import MyApp36 from "./apps/MyApp36.jsx";
+import MyApp37 from "./apps/MyApp37.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <MyApp36 />
+      <MyApp37 />
       <Toaster />
     </Provider>
   </StrictMode>,
