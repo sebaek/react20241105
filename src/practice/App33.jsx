30c68d2fc148bfe0d595f5f74183133a0c5114c9
sebaek <sebaek@gmail.com>
2024-11-07 14:17:01 +0900

    event

--- src/practice/App33.jsx
+++ src/practice/App33.jsx
@@ -6,13 +6,16 @@ function App33(props) {
   return (
     <div>
       {/* input에 keyup 이벤트 발생 시 로그로 메시지 출력 하도록 */}
-      <Input />
+      <Input onKeyUp={() => console.log("키업 이벤트 발생")} />
       <hr />
       {/* 버튼이 클릭되면 로그로 메시지 출력 */}
-      <Button>클릭</Button>
+      <Button onClick={() => console.log("클릭 이벤트 발생")}>클릭</Button>
       <hr />
       {/* textarea에 mouseEnter, mouseLeave 이벤트 발생시 메시지 출력*/}
-      <Textarea />
+      <Textarea
+        onMouseEnter={() => console.log("마우스 들어옴")}
+        onMouseLeave={() => console.log("마우스 떠남")}
+      />
     </div>
   );
 }
