feed3c42c5c63ad3d8955ec2f6e63a7f3a2d1cf3
sebaek <sebaek@gmail.com>
2024-11-07 16:26:02 +0900

    state

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-import App37 from "./practice/App37.jsx";
+import MyApp39 from "./apps/MyApp39.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <App37 />
+      <MyApp39 />
       <Toaster />
     </Provider>
   </StrictMode>,
