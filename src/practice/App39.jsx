464f8ecf1a1eed34e8f56f767799a4d581098ccc
sebaek <sebaek@gmail.com>
2024-11-07 16:37:51 +0900

    state

--- src/practice/App39.jsx
+++ src/practice/App39.jsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../components/ui/button.jsx";
 import { Box } from "@chakra-ui/react";
 
 function App39(props) {
+  const [number, setNumber] = useState(1);
+  let handleButtonClick = () => {
+    setNumber((number % 3) + 1);
+  };
   return (
     <div>
-      <Button>1</Button>
-      <Box h={20} bg={"blue.300"}>
+      <Button onClick={handleButtonClick}>{number}</Button>
+      <Box display={number === 1 ? "block" : "none"} h={20} bg={"blue.300"}>
         1
       </Box>
-      <Box h={20} bg={"red.300"}>
+      <Box display={number === 2 ? "block" : "none"} h={20} bg={"red.300"}>
         2
       </Box>
-      <Box h={20} bg={"yellow.300"}>
+      <Box display={number === 3 ? "block" : "none"} h={20} bg={"yellow.300"}>
         3
       </Box>
     </div>
