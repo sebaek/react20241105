fac7397bf3038509d39822f4b0061015f3ad9a96
sebaek <sebaek@gmail.com>
2024-11-07 09:23:29 +0900

    버튼

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
-import MyApp25 from "./apps/MyApp25.jsx";
+import MyApp26 from "./apps/MyApp26.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <MyApp25 />
+      <MyApp26 />
     </Provider>
   </StrictMode>,
 );
