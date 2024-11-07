9b09b11d6992d28dae2d1ae2e1b438656174a074
sebaek <sebaek@gmail.com>
2024-11-07 14:37:23 +0900

    event 객체

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
-import MyApp34 from "./apps/MyApp34.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
+import MyApp35 from "./apps/MyApp35.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <MyApp34 />
+      <MyApp35 />
       <Toaster />
     </Provider>
   </StrictMode>,
