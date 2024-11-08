fcd037453e51386629e79b88f7a7cd5610b6de46
sebaek <sebaek@gmail.com>
2024-11-08 15:46:45 +0900

    context로 prop drilling 없이 데이터 전달

--- src/practice/App49.jsx
+++ src/practice/App49.jsx
@@ -1,20 +1,27 @@
-import React, { useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 import { Button } from "../components/ui/button.jsx";
 import { Box } from "@chakra-ui/react";
+
+// step1 : context 만들기
+const CountContext = createContext(0);
 
 function App49(props) {
   const [count, setCount] = useState(0);
   return (
     <div>
       <Button onClick={() => setCount(count + 1)}>count {count}</Button>
-      <Child1 count={count} />
+      <CountContext.Provider value={count}>
+        <Child1 count={count} />
+      </CountContext.Provider>
     </div>
   );
 }
-function Child1({ count }) {
-  return <Child2 count={count} />;
+function Child1() {
+  return <Child2 />;
 }
-function Child2({ count }) {
+function Child2() {
+  // step2 : context 사용하기
+  const count = useContext(CountContext);
   return <Box>{count}</Box>;
 }
 
