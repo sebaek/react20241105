a9a3e126a62beb0e64dec3f9c1cc7fac0a5cded2
sebaek <sebaek@gmail.com>
2024-11-08 14:22:05 +0900

    prop drilling

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-import App46 from "./practice/App46.jsx";
+import MyApp47 from "./apps/MyApp47.jsx";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
   <Provider>
-    <App46 />
+    <MyApp47 />
     <Toaster />
   </Provider>,
   // </StrictMode>,
