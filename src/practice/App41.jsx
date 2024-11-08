54f424902189823c372dcda83b3846b7d2dc8e99
sebaek <sebaek@gmail.com>
2024-11-08 10:22:12 +0900

    state 가 객체(object, list)면 새 객체를 만들어서(복사) 상태를 변경해야함

--- src/practice/App41.jsx
+++ src/practice/App41.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Field } from "../components/ui/field.jsx";
 import { Input, Textarea } from "@chakra-ui/react";
 
 function App41(props) {
+  const [book, setBook] = useState({ title: "", content: "" });
+
   return (
     <div>
       <Field label={"제목"}>
-        <Input />
+        <Input onKeyUp={(e) => setBook({ ...book, title: e.target.value })} />
       </Field>
       <Field label={"본문"}>
-        <Textarea />
+        <Textarea
+          onKeyUp={(e) => setBook({ ...book, content: e.target.value })}
+        />
       </Field>
       <hr />
       <Field label={"제목"} readOnly>
