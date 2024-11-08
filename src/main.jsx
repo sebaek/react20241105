9f12ab73a2c17e809493511b5fde792973bb79a0
sebaek <sebaek@gmail.com>
2024-11-08 11:47:50 +0900

    use immer로 깊은 복사를 쉽게

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-import MyApp44 from "./apps/MyApp44.jsx";
+import App44 from "./practice/App44.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <MyApp44 />
+      <App44 />
       <Toaster />
     </Provider>
   </StrictMode>,
