64a2c7cdbc0614777adee0fa4677113c28834cf3
sebaek <sebaek@gmail.com>
2024-11-08 09:48:09 +0900

    state 가 객체(object, list)면 새 객체를 만들어서(복사) 상태를 변경해야함

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-import App40 from "./practice/App40.jsx";
+import MyApp41 from "./apps/MyApp41.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <App40 />
+      <MyApp41 />
       <Toaster />
     </Provider>
   </StrictMode>,
