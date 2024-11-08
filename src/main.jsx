a65042fc88429b80d4e6a121498aee6c532521a5
sebaek <sebaek@gmail.com>
2024-11-08 15:33:44 +0900

    context로 prop drilling 없이 데이터 전달

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-import App48 from "./practice/App48.jsx";
+import MyApp49 from "./apps/MyApp49.jsx";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
   <Provider>
-    <App48 />
+    <MyApp49 />
     <Toaster />
   </Provider>,
   // </StrictMode>,
