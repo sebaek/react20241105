60e74aabc5e0f9d73850afcaf4aa5957144716c8
sebaek <sebaek@gmail.com>
2024-11-08 14:31:08 +0900

    lifting state up

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-import MyApp47 from "./apps/MyApp47.jsx";
+import MyApp48 from "./apps/MyApp48.jsx";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
   <Provider>
-    <MyApp47 />
+    <MyApp48 />
     <Toaster />
   </Provider>,
   // </StrictMode>,
