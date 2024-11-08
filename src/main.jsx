7f49616c13528969a5ce5ca37fa181b141143668
sebaek <sebaek@gmail.com>
2024-11-08 12:30:23 +0900

    state 변경 마다 모두 re-render 하지는 않음

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import React, { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-import App44 from "./practice/App44.jsx";
+import MyApp45 from "./apps/MyApp45.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Provider>
-      <App44 />
-      <Toaster />
-    </Provider>
-  </StrictMode>,
+  // <StrictMode>
+  <Provider>
+    <MyApp45 />
+    <Toaster />
+  </Provider>,
+  // </StrictMode>,
 );
