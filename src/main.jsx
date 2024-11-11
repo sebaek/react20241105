9dd936d6a15e758ba2b5c45a5d1be3fa822dc568
sebaek <sebaek@gmail.com>
2024-11-11 10:52:43 +0900

    useEffect

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-import MyApp53 from "./apps/MyApp53.jsx";
+import MyApp54 from "./apps/MyApp54.jsx";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
   <Provider>
-    <MyApp53 />
+    <MyApp54 />
     <Toaster />
   </Provider>,
   // </StrictMode>,
