9514809a88bb3db44d5cacb8736ea1e16203672a
sebaek <sebaek@gmail.com>
2024-11-11 16:14:43 +0900

    post 요청 보내기 (w/ json)

--- src/practice/App60.jsx
+++ src/practice/App60.jsx
@@ -6,24 +6,38 @@ import {
   NumberInputField,
   NumberInputRoot,
 } from "../components/ui/number-input.jsx";
+import axios from "axios";
 
 function App60(props) {
   const [price, setPrice] = useState(1000);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleButtonClick = () => {
+    axios.post("/api/main1/sub4", {
+      price: price,
+      title: title,
+      content: content,
+    });
+  };
 
   return (
     <div>
       <Field label={"제목"}>
-        <Input />
+        <Input onChange={(e) => setTitle(e.target.value)} value={title} />
       </Field>
       <Field label={"내용"}>
-        <Textarea />
+        <Textarea
+          onChange={(e) => setContent(e.target.value)}
+          value={content}
+        />
       </Field>
       <Field label={"가격"}>
         <NumberInputRoot value={price} onValueChange={(e) => setPrice(e.value)}>
           <NumberInputField />
         </NumberInputRoot>
       </Field>
-      <Button>전송</Button>
+      <Button onClick={handleButtonClick}>전송</Button>
     </div>
   );
 }
