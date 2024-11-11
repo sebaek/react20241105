16acb0a53d7558334f07bcd666e5fc31781a2211
sebaek <sebaek@gmail.com>
2024-11-11 09:31:24 +0900

    context로 prop drilling 없이 데이터 전달
    prop drilling으로 해결

--- src/practice/App51.jsx
+++ src/practice/App51.jsx
@@ -2,25 +2,28 @@ import React, { useState } from "react";
 import { Button } from "../components/ui/button.jsx";
 import { Box } from "@chakra-ui/react";
 
-function MyComp1() {
-  return <MyButton />;
+function MyComp1(props) {
+  return <MyButton {...props} />;
 }
-function MyButton() {
-  const [count, setCount] = useState(0);
-  return <Button onClick={() => setCount(count + 1)}>count : {count}</Button>;
+function MyButton({ count, updateCount }) {
+  return (
+    <Button onClick={() => updateCount(count + 1)}>count : {count}</Button>
+  );
 }
-function MyBox() {
-  const count = 0;
+function MyBox({ count }) {
   return <Box>in box : {count}</Box>;
 }
-function MyComp2() {
-  return <MyBox />;
+function MyComp2({ count }) {
+  return <MyBox count={count} />;
 }
 function App51(props) {
+  const [count, setCount] = useState(0);
+  const updateCount = (c) => setCount(c);
+
   return (
     <div>
-      <MyComp1 />
-      <MyComp2 />
+      <MyComp1 count={count} updateCount={updateCount} />
+      <MyComp2 count={count} />
     </div>
   );
 }
