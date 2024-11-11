2b13b4fecf33999bff84c0f3a8ada02921a2b778
sebaek <sebaek@gmail.com>
2024-11-11 14:21:00 +0900

    axios get 요청

--- src/apps/MyApp58.jsx
+++ src/apps/MyApp58.jsx
@@ -5,8 +5,20 @@ import axios from "axios";
 function MyApp58(props) {
   return (
     <div>
+      <Button>btn3</Button>
       {/* /hello 로  address는 "신촌", city 는 "서울" get 요청 전송*/}
-      <Button onClick={() => {}}>btn2</Button>
+      <Button
+        onClick={() => {
+          axios.get("/hello", {
+            params: {
+              address: "신촌",
+              city: "서울",
+            },
+          });
+        }}
+      >
+        btn2
+      </Button>
       <Button
         onClick={() => {
           axios.get("/hi", {
