ada3068c651335b392a7a91a7c4039aae76a1520
sebaek <sebaek@gmail.com>
2024-11-13 14:41:28 +0900

    location

--- src/apps/MyApp74.jsx
+++ src/apps/MyApp74.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  useNavigate,
+} from "react-router-dom";
+import { Box, Flex } from "@chakra-ui/react";
 
 const router = createBrowserRouter([
   {
@@ -18,6 +24,35 @@ const router = createBrowserRouter([
     ],
   },
 ]);
+function HomeContent() {
+  return <Box>홈 페이지 컨텐츠</Box>;
+}
+function SubContent1() {
+  return <Box> 서브 페이지 1 !!!!!</Box>;
+}
+
+function SubContent2() {
+  return <Box> 서브 페이지 2 @@@@@</Box>;
+}
+function Navbar() {
+  const navigate = useNavigate();
+
+  return (
+    <Flex gap={5}>
+      <Box onClick={() => navigate("/")}>HOME</Box>
+      <Box onClick={() => navigate("/sub1")}>SUB1</Box>
+      <Box onClick={() => navigate("/sub2")}>SUB2</Box>
+    </Flex>
+  );
+}
+function RootPage() {
+  return (
+    <Box>
+      <Navbar />
+      <Outlet />
+    </Box>
+  );
+}
 
 function MyApp74(props) {
   return <RouterProvider router={router} />;
