05b815d1b5895541e4627db31131891ec3199b5b
sebaek <sebaek@gmail.com>
2024-11-13 15:27:18 +0900

    searchParams

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-import MyApp74 from "./apps/MyApp74.jsx";
+import MyApp76 from "./apps/MyApp76.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <MyApp74 />
+      <MyApp76 />
       <Toaster />
     </Provider>
   </StrictMode>,
