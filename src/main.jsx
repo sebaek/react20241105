3c7999487788e2d18ba4640f8c983d314d53b7db
sebaek <sebaek@gmail.com>
2024-11-13 14:14:23 +0900

    link

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-import MyApp71 from "./apps/MyApp71.jsx";
+import App72 from "./practice/App72.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <MyApp71 />
+      <App72 />
       <Toaster />
     </Provider>
   </StrictMode>,
---
+++ src/practice/App72.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// /
+// /add
+// /view
+
+function App72(props) {
+  return <div></div>;
+}
+
+export default App72;
