34941a6db1a649a6ca2a9f96a36599d5564ea935
sebaek <sebaek@gmail.com>
2024-11-13 12:36:12 +0900

    nested router

--- src/apps/MyApp71.jsx
+++ src/apps/MyApp71.jsx
@@ -1,20 +1,42 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom"; // /path3 으로 이동했을 때 어떤 컴포넌트 출력
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom"; // /path3 으로 이동했을 때 어떤 컴포넌트 출력
 
 // /path3 으로 이동했을 때 어떤 컴포넌트 출력
 // /path4 으로 이동했을 때 어떤 컴포넌트 출력
 // /path1/sub1 으로 이동했을 때 어떤 컴포넌트 출력
 // /path1/sub2 으로 이동했을 때 어떤 컴포넌트 출력
+
+// /path3
+// /path3/sub1
+// /path3/sub2
 const router = createBrowserRouter([
   { path: "/", element: <div>여기는 홈 페이지</div> },
   {
     path: "/path1",
-    element: <div>여기는 1번 경로</div>,
+    element: (
+      <div>
+        여기는 1번 경로 <hr />
+        <Outlet />
+      </div>
+    ),
+    children: [
+      { index: true, element: <div>여기는 경로1의 홈페이지</div> },
+      {
+        path: "sub1",
+        element: <div>여기는 경로1/서브1</div>,
+      },
+      {
+        path: "sub2",
+        element: <div>여기는 경로1/서브2</div>,
+      },
+    ],
   },
   {
     path: "/path2",
     element: <div>여기는 2번 경로</div>,
   },
+  { path: "/path3", element: <div>여기는 경로3</div> },
+  { path: "/path4", element: <div>여기는 경로4</div> },
 ]);
 
 function MyApp71(props) {
