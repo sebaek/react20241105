61b8cadbcb0c186087e6c3251f52efd8e0ef8431
sebaek <sebaek@gmail.com>
2024-11-13 11:46:41 +0900

    여러 파일 전송

--- src/practice/App70.jsx
+++ src/practice/App70.jsx
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 function App70(props) {
   const [name, setName] = useState("");
+  const [files, setFiles] = useState([]);
+
+  const handleClick = () => {
+    axios.postForm("/api/main8/sub4", { name: name, attached: files });
+  };
+
   return (
     <div>
       <input
@@ -10,9 +17,9 @@ function App70(props) {
         onChange={(e) => setName(e.target.value)}
       />
       <br />
-      <input type="file" multiple />
+      <input type="file" multiple onChange={(e) => setFiles(e.target.files)} />
       <br />
-      <button>전송</button>
+      <button onClick={handleClick}>전송</button>
     </div>
   );
 }
