14c39d92a6cfe165f5c11916380f0f1fab071500
sebaek <sebaek@gmail.com>
2024-11-13 14:28:47 +0900

    useNavigate

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-import App72 from "./practice/App72.jsx";
+import MyApp73 from "./apps/MyApp73.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <App72 />
+      <MyApp73 />
       <Toaster />
     </Provider>
   </StrictMode>,
