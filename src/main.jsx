65c3b32c8afbd9a731ea3d2d608864a00354215f
sebaek <sebaek@gmail.com>
2024-11-18 16:52:50 +0900

    jwt 예제

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-import MyApp79 from "./apps/MyApp79.jsx";
+import MyApp80 from "./apps/MyApp80.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <MyApp79 />
+      <MyApp80 />
       <Toaster />
     </Provider>
   </StrictMode>,
