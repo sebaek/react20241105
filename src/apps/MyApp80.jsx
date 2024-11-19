2602ad6c04f6ff9902aef1c774d9f8aee62993f7
sebaek <sebaek@gmail.com>
2024-11-19 09:40:49 +0900

    로그인 예제

--- src/apps/MyApp80.jsx
+++ src/apps/MyApp80.jsx
@@ -7,10 +7,39 @@ function MyApp80(props) {
     <div>
       <Button
         onClick={() => {
-          axios.get("/api/main9/sub1");
+          axios.get("/api/main9/sub1").then((res) => {
+            const token = res.data;
+            // console.log(token);
+            localStorage.setItem("token", token);
+          });
         }}
       >
-        jwt 얻기
+        jwt 얻기(로그인)
+      </Button>
+      <Button
+        onClick={() => {
+          localStorage.removeItem("token");
+        }}
+      >
+        로그아웃
+      </Button>
+      <Button
+        onClick={() => {
+          axios.get("/api/main9/sub2");
+        }}
+      >
+        누구나 접근
+      </Button>
+      <Button
+        onClick={() => {
+          axios.get("/api/main9/sub3", {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          });
+        }}
+      >
+        로그인 유저만 접근
       </Button>
     </div>
   );
